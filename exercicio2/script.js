c30f4d0d4016b6bffbccaa8475a53f792bc69f63
Paula Ohashi <paulaohashi@gmail.comgit config --global user.name Paula>
2022-10-18 17:19:24 -0300

    exercicios 1, 2 e 3

---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+let a = prompt("Valor A")
+let b = prompt("Valor B")
+let c
+
+console.log("Valor inicial de A =" , a)
+console.log("Valor inicial de B =" , b)
+
+c = b
+b = a
+a = c
+
+console.log("Valor final de A =" ,a)
+console.log("Valor final de B =" ,b)
